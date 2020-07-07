01b8cf1517c358b39c5247cf83149f864c99a696
EmadKhleifat84 <emad.alikhl@gmail.com>
2020-07-08 01:07:59 +0300

    updated

--- firstsource/test1.java
+++ firstsource/test1.java
@@ -1,6 +1,6 @@
 public class test1{
-	public static void main([argus]){
-	system.output.println("hello world")
+	public static void main(String argus[]){
+	system.output.println("hello world");
 	
 	
 	
